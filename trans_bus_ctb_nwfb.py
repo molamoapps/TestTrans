9c5842e0f46be3e02a86ce4b659a3a61b1637f4a
molamoapps <104378530+molamoapps@users.noreply.github.com>
2022-04-28 13:25:39 +0800

    Update trans_bus_ctb_nwfb.py

--- trans_bus_ctb_nwfb.py
+++ trans_bus_ctb_nwfb.py
@@ -107,7 +107,7 @@ def getRouteStop(co):
                     'dest_tc': route['dest_tc'] if bound == 'outbound' else route['orig_tc'],
                     'dest_sc': route['dest_sc'] if bound == 'outbound' else route['orig_sc'],
                     'stops': route['stops'][bound],
-                    'serviceType': '0'
+                    'service_type': '0'
                 })
 
     with open(ROUTE_LIST, 'w') as f:
