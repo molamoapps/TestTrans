4d08010b51397d3553519099496f23f94b03b499
molamoapps <104378530+molamoapps@users.noreply.github.com>
2022-06-26 23:38:12 +0800

    Update trans_bus_lr.py

--- trans_bus_lr.py
+++ trans_bus_lr.py
@@ -79,13 +79,15 @@ def getRouteStop(co = 'lr'):
     for key, stopMod in stopList.items():
       tmpContainRoute = []
       for routeMod in routeList:
-        if stopMod['stop'] in routeMod['stops']:
-          tmpSeq = routeMod['stops'].index(stopMod['stop'])
-          tmpRoute = {}
-          tmpRoute['ID'] = ('%s%s%s%s%s'%(routeMod['co'], routeMod['route_id'],  routeMod['route'], routeMod['bound'], routeMod.get('service_type', '1')))
-          tmpRoute['i'] = tmpSeq
-          tmpContainRoute.append(tmpRoute)
-      stopMod['routes'] = tmpContainRoute
+          print(stopMod['stop'])
+      #  if stopMod['stop'] in routeMod['stops']:
+      #    tmpSeq = routeMod['stops'].index(stopMod['stop'])
+      #    tmpRoute = {}
+      #    tmpRoute['ID'] = ('%s%s%s%s%s'%(routeMod['co'], routeMod['route_id'],  routeMod['route'], routeMod['bound'], routeMod.get('service_type', '1')))
+      #    tmpRoute['i'] = tmpSeq
+      #    tmpContainRoute.append(tmpRoute)
+      #stopMod['routes'] = tmpContainRoute
+        
 
 
     with open('routeList.lr.json', 'w') as f:
